dd54fd5822e40011db436ea5701671882037f605
2mf3 <yaelmff@gmail.com>
2026-06-23 00:13:42 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 -- ═══ XENLOGIC LOADER ═══
 local gameID = game.PlaceId
 
+-- Asegúrate de que los nombres coincidan exactamente con tus archivos
 local scripts = {
     [97598239454123] = "Grow-a-Garden-2.lua",
     [189707] = "Natural-Disaster-Survival.lua",
@@ -9,7 +10,8 @@ local scripts = {
 local scriptName = scripts[gameID]
 
 if scriptName then
-    local url = "https://raw.githubusercontent.com/2mf3/XenLogic/main/XenLogic%20Scripts/" .. scriptName
+    -- HE CAMBIADO LA RUTA A "XenLogic/" porque es donde tienes tus archivos
+    local url = "https://raw.githubusercontent.com/2mf3/XenLogic/main/XenLogic/" .. scriptName
     
     local success, err = pcall(function()
         loadstring(game:HttpGet(url))()
