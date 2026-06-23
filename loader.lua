3202de9f73d940d3a00228a11082006aafb2a22e
2mf3 <yaelmff@gmail.com>
2026-06-23 00:02:17 -0600

    e

--- loader.lua
+++ loader.lua
@@ -1,16 +1,16 @@
--- main.lua corregido
+-- ═══ XENLOGIC LOADER ═══
 local gameID = game.PlaceId
 
 local scripts = {
     [97598239454123] = "Grow-a-Garden-2.lua",
-    [189707] = "Natural-Disaster-Survival.lua"
-
+    [189707] = "Natural-Disaster-Survival.lua",
 }
 
 local scriptName = scripts[gameID]
 
 if scriptName then
     local success, err = pcall(function()
+        -- URL Raw de tu carpeta de scripts
         local url = "https://raw.githubusercontent.com/2mf3/XenLogic/main/XenLogic%20Scripts/" .. scriptName
         loadstring(game:HttpGet(url))()
     end)
@@ -19,5 +19,5 @@ if scriptName then
         warn("XenLogic Error al cargar: " .. tostring(err))
     end
 else
-    warn("XenLogic: No hay script para este juego (ID: " .. gameID .. ")")
+    warn("XenLogic: No hay script para este ID de juego (ID: " .. gameID .. ")")
 end
